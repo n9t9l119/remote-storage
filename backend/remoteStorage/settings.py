9c040671fe79884949fbb7fcf3dd1bd0aa2b0a11
n9t9l119 <n9t9l119@gmail.com>
2021-12-19 14:14:37 +0300

    настроена база в докере!

--- backend/remoteStorage/settings.py
+++ backend/remoteStorage/settings.py
@@ -92,11 +92,11 @@ WSGI_APPLICATION = 'remoteStorage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dna',
+        'USER': 'user',
+        'PASSWORD': '123456',
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
         'PORT': 5432,
     }
 }
